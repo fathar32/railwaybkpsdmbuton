64deddd2ccb3b48d8a1177b966bb4747cbe7eb94
fathar32 <117836565+fathar32@users.noreply.github.com>
2025-11-16 00:28:33 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,17 +1,9 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT),
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Railway/Supabase butuh SSL
 });
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('DB connection error:', err);
-  } else {
-    console.log('DB connected at:', res.rows[0].now);
-  }
-});
+
+export default pool;

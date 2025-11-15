000d32de6a16d53e8cdc7b2ad4661ecac05081b6
fathar32 <117836565+fathar32@users.noreply.github.com>
2025-11-16 02:08:44 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors({ origin: process.env.FRONTEND_ORIGIN || 'https://verifbkpsdmbuton09.vercel.app/' }));
 
 app.get('/health', (req, res) => res.json({ ok: true }));
-app.use('/api/verifikasi', verifikasiRouter);
+app.use('/api/verfikasi', verifikasiRouter);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Backend running on port ${port}`));
